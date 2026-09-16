4e6e6264c54274b3be3d3313e57519bae051334d
lucasvmigotto <lvmigotto1@gmail.com>
2026-09-16 14:54:40 -0300

    fix(devutils): error when env file is missing

--- scripts/.dev.utils.sh
+++ scripts/.dev.utils.sh
@@ -4,8 +4,15 @@ function loadenv() {
 
     local env_file=${1:-'.env'}
 
+    if [[ ! -f "${env_file}" ]]; then
+        echo "File ${env_file} not found" >&2
+        return 1
+    fi
+
     set -a
     eval "$(cat "${env_file}")"
     set +a
+
+    return 0
 
 }
